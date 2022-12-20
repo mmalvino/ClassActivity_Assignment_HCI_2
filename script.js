874bbfa4498eff6daf3fe3b7e2466c0a0fb85391
Micky Malvino <114371813+mmalvino@users.noreply.github.com>
2022-12-20 22:59:47 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Add your answer here
+$(function() {
+    $('.lesson').hover(
+      function() {
+        $(this).find('.text-contents').addClass('text-active');
+      },
+      function() {
+        $(this).find('.text-contents').removeClass('text-active');
+      }
+    );
+  
+  });
+  
+  
